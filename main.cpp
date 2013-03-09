26bc122601be987e4ea23406c80ef9c2d34ce2ac
Harald Zealot <harald_zealot@tut.by>
2013-03-09 10:23:34 +0300

    histogram for chi-squared

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
    for (int i = 0; i < 20; ++i)
    {
       cout.width (3);
-      cout << (int)randInt8() << endl;
+      cout << (int) randInt8() << endl;
    }
 
    cout << "\nSmall random number sequence 16 bit:\n";
@@ -36,6 +36,29 @@ int main()
       cout.width (20);
       cout << randInt64() << endl;
    }
+
+   uint16_t limits[5] = {255, 16383, 32767, 49151, 65535};
+   int histogram[5] = {0, 0, 0, 0, 0};
+
+   for (int i = 0; i < 4000; ++i)
+   {
+      uint16_t rnd = randInt16();
+
+      for (int j = 0; j < 5; ++j)
+      {
+         if (rnd <= limits[j])
+         {
+            histogram[j]++;
+            break;
+         }
+      }
+   }
+
+   cout << endl << endl;
+
+   for (int i = 0; i < 5; ++i)
+      cout << limits[i] << ":  " << histogram[i] << endl;
+
 
    return 0;
 }
